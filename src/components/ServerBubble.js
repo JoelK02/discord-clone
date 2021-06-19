bc9027b2305855dc60f9842be4920f25ab1c5451
JoelK02 <kwohlikxun@hotmail.com>
2021-06-19 15:37:51 +0800

    fixed serverbubble icon centering

--- src/components/ServerBubble.js
+++ src/components/ServerBubble.js
@@ -3,6 +3,7 @@ import { Avatar } from "@material-ui/core";
 import "./ServerBubble.css";
 import { useDispatch } from "react-redux";
 import { setServerInfo } from "../features/appSlice";
+import GroupIcon from "@material-ui/icons/Group";
 
 function ServerBubble({ id, serverName, serverImage }) {
   const dispatch = useDispatch();
@@ -22,6 +23,9 @@ function ServerBubble({ id, serverName, serverImage }) {
     >
       <div className="bubble">
         <img src={serverImage} alt="" />
+        <div className="letter">
+          <GroupIcon />
+        </div>
       </div>
     </div>
   );
